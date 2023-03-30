380f7252af38d227e53cf10211edc63783f31270
Danefron <86188096+Danefron@users.noreply.github.com>
2023-03-30 17:59:39 +0530

    Update BuildPlayer.cs

--- src/PluginTemplate/BuildPlayer.cs
+++ src/PluginTemplate/BuildPlayer.cs
@@ -82,7 +82,7 @@ namespace BuildMaster
 		}
 
 		// Token: 0x06000016 RID: 22 RVA: 0x000021F0 File Offset: 0x000003F0
-		public void Leave(LeaveRoomArgs room)
+		public void Leave()
 		{
 			BuildRoom roomByID = ConfigUtils.GetRoomByID(base.CurrentRoomID);
 			if (roomByID == null)
