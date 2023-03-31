f5fe35f63fec2a51b5bc6d4f2accb598e37b498b
Danefron <86188096+Danefron@users.noreply.github.com>
2023-03-31 14:39:25 +0530

    Update BuildPlayer.cs

--- src/PluginTemplate/BuildPlayer.cs
+++ src/PluginTemplate/BuildPlayer.cs
@@ -57,7 +57,7 @@ namespace BuildMaster
 		// Token: 0x06000015 RID: 21 RVA: 0x00002150 File Offset: 0x00000350
 		public void Join()
 		{
-			if (room.GetPlayerCount() >= room.MaxPlayer)
+			if (BuildRoom room.GetPlayerCount() >= room.MaxPlayer)
 			{
 				this.SendInfoMessage("此房间满人了");
 				return;
@@ -72,12 +72,12 @@ namespace BuildMaster
 				this.Leave();
 			}
 			base.Join(room.ID);
-			room.Players.Add(this);
-			room.Broadcast("玩家 " + base.Name + " 加入了房间", Color.MediumAquamarine);
-			this.Teleport(room.WaitingPoint);
-			if (!room.Loaded)
+			BuildRoom room.Players.Add(this);
+			BuildRoom room.Broadcast("玩家 " + base.Name + " 加入了房间", Color.MediumAquamarine);
+			this.Teleport(BuildRoom room.WaitingPoint);
+			if (!BuildRoom room.Loaded)
 			{
-				room.LoadRegion();
+				BuildRoom room.LoadRegion();
 			}
 		}
 
